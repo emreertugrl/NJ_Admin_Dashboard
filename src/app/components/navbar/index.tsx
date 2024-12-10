49797ef9009f74e267fdbe99728b5d6bc1690ee6
emreertugrl <emrertugrl@gmail.com>
2024-12-10 18:31:09 +0300

    navbar completed

--- src/app/components/navbar/index.tsx
+++ src/app/components/navbar/index.tsx
@@ -1,5 +1,26 @@
+import { sections } from "@/app/utils/constants";
+import { RxHamburgerMenu } from "react-icons/rx";
+import NavLink from "./NavLink";
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <nav className="h-screen min-w-[60px]">
+      <div className="navbar flex flex-col gap-5 text-gray-500 fixed bg-white h-screen z-50">
+        <button className="grid place-items-center pt-5 text-2xl">
+          <input type="checkbox" name="menu" id="menu" />
+          <label htmlFor="menu">
+            <RxHamburgerMenu />
+          </label>
+        </button>
+
+        <div>
+          {sections.map((i, key) => (
+            <NavLink item={i} key={key} />
+          ))}
+        </div>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
