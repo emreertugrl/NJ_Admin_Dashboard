114df3f2e25d6a5976d649f5520ee3e0a23cb910
emreertugrl <emrertugrl@gmail.com>
2024-12-16 13:58:01 +0300

    orders page render to screen

--- src/app/(pages)/orders/page.tsx
+++ src/app/(pages)/orders/page.tsx
@@ -1,5 +1,15 @@
-const Orders = () => {
-  return <div>Orders</div>;
+import OrdersTable from "@/app/components/table/OrdersTable";
+import { Order } from "@/app/types";
+import { getOrders } from "@/app/utils/api";
+
+const Orders = async () => {
+  const orders: Order[] = await getOrders();
+  return (
+    <div>
+      <h1 className="title">Siparişler</h1>
+      <OrdersTable orders={orders} />
+    </div>
+  );
 };
 
 export default Orders;
